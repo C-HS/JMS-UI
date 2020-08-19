a694e5eeaa4882b7a6709855d2f01bb79e8640ee
zafar <zafar.parvez@gmail.com>
2020-08-19 13:15:54 +0530

    With updated KML plotting.

---
+++ shipyard/container yard/sample-geojson.js
@@ -0,0 +1,27 @@
+var customeBondedArea = {
+	"type":"FeatureCollection",
+	"features":[{
+		"type":"Feature",
+		"geometry":{
+			"type":"Polygon",
+			"coordinates":[[
+				[80.2457953,26.4585406,0],
+				[80.2457953,26.4579739,0],
+				[80.2463103,26.4579355,0],
+				[80.2462674,26.4574937,0],
+				[80.2484346,26.45734,0],
+				[80.2485848,26.4583293,0],
+				[80.2457953,26.4585406,0]
+			]]
+		},
+		"properties":{
+			"name":"Customs Bonded Area"
+		}
+	}]
+};
+var emptyContainerYard = {"type":"FeatureCollection","features":[
+{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[80.2428771,26.4588576,0],[80.2428127,26.4583005,0],[80.2456344,26.4580796,0],[80.2456559,26.4586367,0],[80.2428771,26.4588576,0]]]},"properties":{"name":"Empty Container Yard"}}
+]};
+var railwayYard = {"type":"FeatureCollection","features":[
+{"type":"Feature","geometry":{"type":"Polygon","coordinates":[[[80.2422012,26.4591553,0],[80.2421797,26.4589248,0],[80.2485956,26.4583965,0],[80.2486492,26.4586463,0],[80.2422012,26.4591553,0]]]},"properties":{"name":"Railway Yard"}}
+]};
